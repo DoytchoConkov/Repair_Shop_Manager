02b0125a5580fadbc689d2527c61b782da791d64
DoytchoConkov <conkov.doytcho@gmail.com>
2021-03-20 23:21:45 +0200

    Start creating tests

---
+++ src/test/java/mainPackage/services/BrandServiceTest.java
@@ -0,0 +1,16 @@
+package mainPackage.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BrandServiceTest {
+
+    @Test
+    void getBrand() {
+    }
+
+    @Test
+    void getAll() {
+    }
+}
