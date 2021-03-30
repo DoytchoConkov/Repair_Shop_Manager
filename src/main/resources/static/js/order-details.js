06b63b05bb141001dd1615752188fd98fa814396
DoytchoConkov <conkov.doytcho@gmail.com>
2021-03-31 00:36:45 +0300

    Start JS for zooming image part 2.

--- src/main/resources/static/js/order-details.js
+++ src/main/resources/static/js/order-details.js
@@ -1,5 +1,13 @@
 let image = $('#image');
-
-image.click(()=>{
-    image.style.height=600;
+let clicked = false;
+image.click(() => {
+    if (clicked) {
+        image[0].style.height = "300px";
+        clicked=false;
+    } else {
+        image[0].style.height = "600px";
+        image[0].style.marginLeft="auto";
+        image[0].style.marginRight="auto";
+        clicked=true;
+    }
 })
