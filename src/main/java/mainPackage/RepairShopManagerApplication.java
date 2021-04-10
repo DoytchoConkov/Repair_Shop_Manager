8102cf2f9129c79a1fd2beb3cfacf338ed55282e
DoytchoConkov <conkov.doytcho@gmail.com>
2021-04-10 17:37:02 +0300

    Some cosmetics.

--- src/main/java/mainPackage/RepairShopManagerApplication.java
+++ src/main/java/mainPackage/RepairShopManagerApplication.java
@@ -2,8 +2,10 @@ package mainPackage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class RepairShopManagerApplication {
 
     public static void main(String[] args) {
